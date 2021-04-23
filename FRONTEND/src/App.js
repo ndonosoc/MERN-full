dc47a36aa966bee8a32e459cbfe8b3fb814cf9d1
Nicolas Donoso <nicodonosocastex@hotmail.com>
2021-04-23 16:38:48 -0300

    Added custom buttons

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 // Redirect redirecciona, y switch hace que esa redireccion pase solo si no toma alguna de los path anteriores
@@ -15,6 +16,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places">
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
